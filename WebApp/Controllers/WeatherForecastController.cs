195e77b3f37c8790ab92a832c1027ee022937992
Nuwantha Sandakelum <nuwantha@calcey.com>
2023-09-17 21:37:47 +0530

    added new feature

--- WebApp/Controllers/WeatherForecastController.cs
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApp.Controllers
   {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Windy"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Windy", "Hot"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
